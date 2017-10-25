484904d30e5595c64dc08b6b164051d704c7fad6
sgehin <sfa.gehin@gmail.com>
2017-10-25 19:55:38 +0200

    basisberekening uurtarief

--- index.php
+++ index.php
@@ -1,29 +1,19 @@
 <?php
-    $start = 9; 
-   $stop = 14;
-   $parkeertijd = $stop - $start;
-   $prijs = 2.5;
-   $tarief = $parkeertijd * $prijs;
-   $tebetalen = 12.5;
-        echo "Starttijd is:".$start."uur"."<br>";
-    echo "Stoptijd is:".$stop."uur"."<br>";
-    echo "Parkeertijd is:".$parkeertijd."uur"."<br>"."<br>";
-    echo "Het te betalen bedrag is:".$tarief." "."Euro"."<br>"."<br>";
-    if($tebetalen <$tarief){
-        echo "Bedrag is onvoldoende. Slagboom blijft gesloten"."<br>";
-    }else if($tebetalen == $tarief){
-        echo"Dank u voor de betaling. U kunt uitrijden"."<br>";
-           }else {
-        echo "Restant krijgt u terug"."<br>";
-       /* $d=mktime(11, 14, 54, 8, 12, 2014);
-            echo "Created date is " . date("h:i:sa", $d)."<br>";
-        $e= mktime(12,00,03, 8, 12, 2014);
-            echo "nieuwe tijd is " . date("h:i:sa", $e)."<br>";
-            
-            $verschil = date("h:i:sa", $e) - mktime(12,00,03, 8, 12, 2014);
-                echo "$verschil";
-        */
-    }
-       
-?>   
+ //ophalen tijd 
+$StartTijd=1508949042;
+  $VerstrekenTijd = time()-$StartTijd;
+    echo $StartTijd;
+    echo "<br>"."<br>";
+    echo $VerstrekenTijd;
+    echo "<br>"."<br>";
     
+//te betalen bedrag
+  $Tarief = .75;  
+    
+   $uur1 = ($VerstrekenTijd/(60*60))*$Tarief;
+   echo $uur1;
+   
+    
+    
+    
+  ?>
