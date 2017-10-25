90e74f8259b3c2a5e8268efd471ed7969dbe1aac
sgehin <sfa.gehin@gmail.com>
2017-10-25 20:17:22 +0200

    3 uurtarieven afgerond op 2 decimalen

--- index.php
+++ index.php
@@ -8,12 +8,15 @@ $StartTijd=1508949042;
     echo "<br>"."<br>";
     
 //te betalen bedrag
-  $Tarief = .75;  
+  $Tarief = array(.75,.50,.25);  
     
-   $uur1 = ($VerstrekenTijd/(60*60))*$Tarief;
-   echo $uur1;
-   
+   $uur1 = ($VerstrekenTijd/(60*60))*$Tarief[0];
+   echo round($uur1,2)."<br>";
+   $uur2 = ($VerstrekenTijd/(60*60))*$Tarief[1];
+   echo round($uur2,2)."<br>";
+   $uur3 = ($VerstrekenTijd/(60*60))*$Tarief[2];
+   echo round($uur3,2)."<br>";
     
     
     
-  ?>
+  ?> 
